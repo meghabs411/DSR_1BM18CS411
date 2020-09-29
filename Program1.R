3b5a66fa06902ed8a6f448111b7b931028b43a0f
meghabs411 <53851853+meghabs411@users.noreply.github.com>
2020-09-29 17:59:19 +0530

    Create Program1.R

---
+++ Program1.R
@@ -0,0 +1,7 @@
+@@ -0,0 +1,7 @@
+#1)Write a R code to create a list of data types :
+N1<-c(10,20,30,40)
+Str1<-c("Water","Lemon","Juice")
+Rlnum<-c(23.3,44.5,67.7,89.0)
+List1<-list(N1,Str1,Rlnum)
+List1
