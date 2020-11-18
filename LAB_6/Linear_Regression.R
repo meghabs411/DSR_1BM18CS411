661624e05c6f0904244dac603f81b6f2759d8001
meghabs411 <53851853+meghabs411@users.noreply.github.com>
2020-11-18 14:54:02 +0530

    Create Linear_Regression.R

---
+++ LAB_6/Linear_Regression.R
@@ -0,0 +1,31 @@
+# Linear Regression 
+
+count <-c(9,25,15,2,14,25,24,47)
+speed<-c(2,3,5,9,14,24,29,34)
+fw<-data.frame(count,speed)
+fw
+cor(count, speed)
+names<-c("Taw","Torridge","Ouse", "Exe","Lyn","Brook","Ditch","Fal")
+rownames(fw)=names
+fw
+fw.lm=lm(count ~ speed, data = fw)
+summary(fw.lm)
+names(fw.lm)
+fw.lm$coefficients
+
+#Gives slope and intercept
+newypred<-fitted(fw.lm)
+newypred
+#obtaining confidence intervals
+confint(fw.lm)
+confint(fw.lm, parm=c('(Intercept)','speed'),level=0.9)
+#fitted values
+fitted(fw.lm)
+residuals(fw.lm)
+#plotting the x, y values
+plot(fw$speed, fw$count, col="red")
+coef(fw.lm)
+#plotting the fitted line
+abline(coef(fw.lm),lty=1,col="blue")
+#plotting residuals
+plot(fw.lm, which=1)
